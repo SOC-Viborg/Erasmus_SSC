d054c623bd0e74c2c470e530bf5ae977203a4902
Tetiana-lab <tania.reshetylova@gmail.com>
2025-11-25 14:31:39 +0100

    Add: login page, admin dashboard; Fix: home, mainLayout

---
+++ Erasmus_SSC/Erasmus_SSC.Client/ViewModels/UserViewModelcs.cs
@@ -0,0 +1,9 @@
+﻿namespace Erasmus_SSC.Client.ViewModels
+{
+    public class UserViewModelcs
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
